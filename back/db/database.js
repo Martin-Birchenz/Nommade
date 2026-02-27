f450f610bf3b9d724e1d647236bdb354cd86c9dd
Birchenz Martín <birchenzmartin@gmail.com>
2026-02-27 17:14:05 -0300

    Pequeños detalles.

--- back/db/database.js
+++ back/db/database.js
@@ -9,7 +9,7 @@ const getConnection = async () => {
     database: process.env.DB_DATABASE,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    port: process.env.PORT || 3360,
+    port: process.env.DB_PORT || 3306,
   });
 };
 
