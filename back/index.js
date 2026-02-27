c2b29a6d6555f4f0810ffbaac14c09d921db4681
Birchenz Martín <birchenzmartin@gmail.com>
2026-02-27 18:28:47 -0300

    Cambiamos el puerto

--- back/index.js
+++ back/index.js
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
 });
 
 // Puerto
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Listening in port ${PORT}! http://localhost:${PORT}/login`);
