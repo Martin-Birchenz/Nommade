b64077e53614f30daa5347cd55d79bb5618c82ee
Birchenz Martín <birchenzmartin@gmail.com>
2026-02-27 17:56:20 -0300

    Otros cambios.

--- back/index.js
+++ back/index.js
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Listening in port ${PORT}! http://localhost:3000/login`);
+  console.log(`Listening in port ${PORT}! http://localhost:${PORT}/login`);
 });
 
 // Rutas
