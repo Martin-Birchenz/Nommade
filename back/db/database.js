0b2e527bbfbf62265d59604fb052159d302d174c
Birchenz Martín <birchenzmartin@gmail.com>
2026-02-28 15:39:54 -0300

    Cambiamos a 8080.

--- back/db/database.js
+++ back/db/database.js
@@ -9,7 +9,7 @@ const getConnection = async () => {
     database: process.env.DB_DATABASE,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT || 8080,
   });
 };
 
