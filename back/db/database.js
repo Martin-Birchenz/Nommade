765d6c4a160aaf8cccf2ad9613c97754ba41380f
Birchenz Martín <birchenzmartin@gmail.com>
2026-02-28 15:30:02 -0300

    Va a andar

--- back/db/database.js
+++ back/db/database.js
@@ -4,14 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const getConnection = async () => {
-  const connectionUrl =
-    "mysql://root:FEJQtQzcRFhVCFNBgyhHbwNSLYdUFQKN@yamanote.proxy.rlwy.net:56719/railway";
   return await mysql.createConnection({
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT || 8080,
+    port: process.env.DB_PORT || 3306,
   });
 };
 
