3d8610ac634cc1ceb57b5ceb6bc0a3ac1afd0db2
Birchenz Martín <birchenzmartin@gmail.com>
2026-02-28 17:11:25 -0300

    Página desplegada! Haciendo cambios en el login.

--- back/index.js
+++ back/index.js
@@ -20,7 +20,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://nommade-production.up.railway.app",
+    origin: process.env.PUBLIC_URL,
     credentials: true,
   }),
 );
--- back/src/authentication.js
+++ back/src/authentication.js
@@ -34,16 +34,16 @@ async function login(req, res) {
         .send({ status: "Error", message: "Error durante login" });
     }
 
-    const expira = parseInt(process.env.JWT_COOKIE_EXP) || 1;
-
-    const secret = process.env.JWT_SECRET || "secreto";
-
-    const token = jsonWebToken.sign({ user: usuario.user }, secret, {
-      expiresIn: "1d",
-    });
+    const token = jsonWebToken.sign(
+      { user: usuario.user },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d",
+      },
+    );
 
     const cookieOption = {
-      expires: new Date(Date.now() + expira * 24 * 60 * 60 * 1000),
+      expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
       path: "/",
       httpOnly: true,
       sameSite: "none",
