d4b0936e6cc6efcb93b739bd0476f525f7611df8
Birchenz Martín <birchenzmartin@gmail.com>
2026-02-28 15:51:28 -0300

    Cambios de variables.

--- back/db/database.js
+++ back/db/database.js
@@ -4,13 +4,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const getConnection = async () => {
-  return await mysql.createConnection({
+  const connection = process.env.MYSQL_URL || {
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT || 8080,
-  });
+    port: process.env.DB_PORT || 3306,
+  };
+  return await mysql.createConnection(connection);
 };
 
 export { getConnection };
--- back/index.js
+++ back/index.js
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
 // Puerto
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Listening in port ${PORT}! http://localhost:${PORT}:/login`);
 });
 
