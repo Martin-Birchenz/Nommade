5a4b118d087ae32969e9cec14fa90e1169828935
Martin Birchenz <martinbirchenz@Martins-MacBook-Neo.local>
2026-05-27 16:44:44 -0300

    Cambios en el servidor

--- back/db/database.js
+++ back/db/database.js
@@ -3,15 +3,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const getConnection = async () => {
-  const connection = process.env.MYSQL_URL || {
+const pool = mysql.createPool({
     host: process.env.MYSQLHOST || process.env.DB_HOST,
     database: process.env.MYSQLDATABASE || process.env.DB_DATABASE,
     user: process.env.MYSQLUSER || process.env.DB_USER,
     password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
     port: process.env.MYSQLPORT || 3306,
-  };
-  return await mysql.createConnection(connection);
-};
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+})
 
-export { getConnection };
+export async function getConnection() {
+  return await pool.getConnection()
+}
+
