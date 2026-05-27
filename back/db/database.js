5f55a56d9c943ee3b7f81b914c69bd1a59b5c5b5
Martin Birchenz <martinbirchenz@Martins-MacBook-Neo.local>
2026-05-27 17:25:52 -0300

    Intentado revivir la base de datos

--- back/db/database.js
+++ back/db/database.js
@@ -4,11 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql.createPool({
-    host: process.env.MYSQLHOST || process.env.DB_HOST,
-    database: process.env.MYSQLDATABASE || process.env.DB_DATABASE,
-    user: process.env.MYSQLUSER || process.env.DB_USER,
-    password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
-    port: process.env.MYSQLPORT || 3306,
+    host: process.env.MYSQLHOST || process.env.MYSQLHOST.env.DB_HOST,
+    database: process.env.MYSQLDATABASE  || process.env.MYSQLDATABASE.env.DB_DATABASE,
+    user: process.env.MYSQLUSER  || process.env.MYSQLUSER.env.DB_USER,
+    password: process.env.MYSQLPASSWORD  || process.env.MYSQLPASSWORD.env.DB_PASSWORD,
+    port: process.env.MYSQLPORT || 3306 || process.env.MYSQLPORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
@@ -17,4 +17,3 @@ const pool = mysql.createPool({
 export async function getConnection() {
   return await pool.getConnection()
 }
-
